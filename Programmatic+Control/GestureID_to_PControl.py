0e44e4eeff4f259309fb5edf0e7cf6e70393c9aa
Toni Pano <toni.pano19@gmail.com>
2021-03-17 20:56:06 -0400

    Added a finite state machine to the Gesture ID -> Programmatic control mapping. It is done as a class to keep track of its internal state variables.

--- Programmatic+Control/GestureID_to_PControl.py
+++ Programmatic+Control/GestureID_to_PControl.py
@@ -1,7 +1,11 @@
 import time
 import Keyboard
 import Mouse
+from GestureControl import gestures
 
+idToGesture = dict((id, gestureText) for (gestureText, id) in gestures.items())
+
+"""
 gestures = {
     0 : "Move Mouse",
     1 : "## Press Left (replaced Drag)",
@@ -10,67 +14,104 @@ gestures = {
     4 : "Left Click",
     5 : "Right Click",
     6 : "## Toggle Keyboard Visibility (not a gesture yet)",
-}
+}"""
 
-m = Mouse.Mouse()
-kb = Keyboard.Keyboard()
+class FSM:
+    def __init__(self, mouse, keyboard):
+        self.m = mouse
+        self.kb = keyboard
+        self.isDrag = False
 
-def controlComputer(gestureID, x = None, y = None,):
+        self.inputs = [-1,-1,-1,-1,-1]
+        self.i = 0
 
-    if(gestureID == 0):     # move mouse (dynamic)
-        m.update(x = x, y = y)
-
+    def controlComputer(self, gestureID, x = None, y = None,):
         """
-        # just press/release left click when needed (2) and move mouse to replace drag function
-        elif(gestureID == 1):   # drag (left click+ hold, dynamic)
-            if(hold == False):
-                hold == True
-                m.update(pressLeft=True)
+        #only let gesture ID through if previous 5 ids match
+        self.inputs[self.i] = gestureID
+        allMatch = True
+        for i in inputs:
+            if(i != gestureID):
+                allMatch = False
+                break
+
+        #do not process input if id numbers not stable enough
+        if(allMatch == False):
+            return
+        """
+
+        # account for drag having 2 states, the press and release of left click
+        if(gestureID == 4): # press left click for drag
+            if(self.isDrag):
+                self.m.update(x=x, y=y)
             else:
-    
-        """
+                self.isDrag = True
+                self.m.update(pressLeft = True, x=x, y=y)
 
-    elif(gestureID == 1): # press left click
-        m.update(pressLeft = True)
+        else:
+            if(self.isDrag): # release left click from drag
+                isDrag = False
+                self.m.update(pressLeft = False)
 
-    elif(gestureID == 2): # release left click
-        m.update(pressLeft = False)
+            else:
 
-    elif(gestureID == 3):   # double left click (static)
-        m.update(pressLeft = True)
-        time.sleep(0.01)
-        m.update(pressLeft = False)
-        time.sleep(0.01)
-        m.update(pressLeft = True)
-        time.sleep(0.01)
-        m.update(pressLeft = False)
+                if(gestureID == 0):     # move mouse (dynamic)
+                    self.m.update(x = x, y = y)
 
-    elif(gestureID == 4):   # left click (static)
-        m.update(pressLeft = True)
-        time.sleep(0.01)
-        m.update(pressLeft = False)
+                    """
+                    # just press/release left click when needed (2) and move mouse to replace drag function
+                    elif(gestureID == 1):   # drag (left click+ hold, dynamic)
+                        if(hold == False):
+                            hold == True
+                            m.update(pressLeft=True)
+                        else:
+                
+                    """
 
-    elif(gestureID == 5):   # right click (static)
-        m.update(pressRight = True)
-        time.sleep(0.01)
-        m.update(pressRight = False)
+                elif(gestureID == 1):   # left click (static)
+                    self.m.update(pressLeft = True)
+                    time.sleep(0.01)
+                    self.m.update(pressLeft = False)
 
-    elif(gestureID == 6): # show/hide keyboard keyboard
-        kb.updateKey("CTRL", pressDown = True)
-        kb.updateKey("LEFT WINDOWS", pressDown = True)
-        kb.updateKey("O", pressDown = True)
-        kb.updateKey("O", pressDown=False)
-        kb.updateKey("LEFT WINDOWS", pressDown=False)
-        kb.updateKey("CTRL", pressDown=False)
+                elif (gestureID == 2):  # double left click (static)
+                    self.m.update(pressLeft=True)
+                    time.sleep(0.01)
+                    self.m.update(pressLeft=False)
+                    time.sleep(0.01)
+                    self.m.update(pressLeft=True)
+                    time.sleep(0.01)
+                    self.m.update(pressLeft=False)
 
-    else:
-        m.update(pressLeft = False, pressRight = False)
-        print("Gesture ID "+str(gestureID)+" not recognized")
+                elif (gestureID == 3):  # right click (static)
+                    self.m.update(pressRight=True)
+                    time.sleep(0.01)
+                    self.m.update(pressRight=False)
+
+                elif(gestureID == 5): # show/hide keyboard keyboard
+                    self.kb.updateKey("CTRL", pressDown = True)
+                    self.kb.updateKey("LEFT WINDOWS", pressDown = True)
+                    self.kb.updateKey("O", pressDown = True)
+                    self.kb.updateKey("O", pressDown=False)
+                    self.kb.updateKey("LEFT WINDOWS", pressDown=False)
+                    self.kb.updateKey("CTRL", pressDown=False)
+
+                else:
+                    self.m.update(pressLeft = False, pressRight = False)
+                    print("Gesture ID "+str(gestureID)+" not recognized")
 
 if __name__ == "__main__":
     print("Hi")
-    controlComputer(6) #show keyboard
+    m = Mouse.Mouse()
+    kb = Keyboard.Keyboard()
+    fsm = FSM(m,kb)
+    fsm.controlComputer(5) #show keyboard
     time.sleep(1)
-    controlComputer(4) #left click
+    fsm.controlComputer(1) #left click
     time.sleep(1)
-    controlComputer(6) #show keyboard
+    fsm.controlComputer(5) #hide keyboard
+    time.sleep(1)
+    fsm.controlComputer(4,0,0)
+    time.sleep(1)
+    fsm.controlComputer(4, 100, 100)
+    time.sleep(1)
+    fsm.controlComputer(0, 200, 200)
